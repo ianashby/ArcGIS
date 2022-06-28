767f69a20713599332cef5f56385c738eb1f932c
ianashby <ianashbyphotography@gmail.com>
2022-06-27 20:32:36 -0600

    06/27

--- map.js
+++ map.js
@@ -1,106 +1,64 @@
 require([
-    "esri/config",
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/Graphic",
-    "esri/layers/GraphicsLayer"
-], function (esriConfig, Map, MapView, Graphic, GraphicsLayer) {
+   "esri/config",
+   "esri/Map",
+   "esri/views/MapView",
+   "esri/layers/FeatureLayer",
+], function (esriConfig, Map, MapView, FeatureLayer) {
 
-    esriConfig.apiKey = "AAPKf250a14308274e28a7e1fe2f8385106dSMRJ02w3oLidoyW1UMRrNAP-vA5EBsxB2P6nLjbQWkkGkncF1tTn5G7LbcL0nmPI";
+   // This API Key should be kept private. Please Insert your own API Key here.
+   esriConfig.apiKey = API_KEY;
 
-    const map = new Map({
-        basemap: "arcgis-topographic",
-    });
+   // Create a new map using the Newspaper template/theme.
+   const map = new Map({
+       basemap: "arcgis-newspaper",
+   });
 
-    const view = new MapView({
-        map: map,
-        center: [-118.80500,34.02700], //Longitude, latitude
-        zoom: 13,
-        container: "viewDiv"
-     });
-    
-     const graphicsLayer = new GraphicsLayer();
-     map.add(graphicsLayer);
-    
-     const point = { //Create a point
-        type: "point",
-        longitude: -118.80657463861,
-        latitude: 34.0005930608889
-     };
-     const simpleMarkerSymbol = {
-        type: "simple-marker",
-        color: [226, 119, 40],  // Orange
-        outline: {
-            color: [255, 255, 255], // White
-            width: 1
-        }
-     };
-    
-     const pointGraphic = new Graphic({
-        geometry: point,
-        symbol: simpleMarkerSymbol,
-     });
-     graphicsLayer.add(pointGraphic);
-    
-        // Create a line geometry
-     const polyline = {
-        type: "polyline",
-        paths: [
-            [-118.821527826096, 34.0139576938577], //Longitude, latitude
-            [-118.814893761649, 34.0080602407843], //Longitude, latitude
-            [-118.808878330345, 34.0016642996246]  //Longitude, latitude
-        ]
-     };
-     const simpleLineSymbol = {
-        type: "simple-line",
-        color: [226, 119, 40], // Orange
-        width: 2
-     };
-    
-     const polylineGraphic = new Graphic({
-        geometry: polyline,
-        symbol: simpleLineSymbol
-     });
-     graphicsLayer.add(polylineGraphic);
-    
-     // Create a polygon geometry
-     const polygon = {
-        type: "polygon",
-        rings: [
-            [-118.818984489994, 34.0137559967283], //Longitude, latitude
-            [-118.806796597377, 34.0215816298725], //Longitude, latitude
-            [-118.791432890735, 34.0163883241613], //Longitude, latitude
-            [-118.79596686535, 34.008564864635],   //Longitude, latitude
-            [-118.808558110679, 34.0035027131376]  //Longitude, latitude
-        ]
-     };
-    
-     const simpleFillSymbol = {
-        type: "simple-fill",
-        color: [227, 139, 79, 0.8],  // Orange, opacity 80%
-        outline: {
-            color: [255, 255, 255],
-            width: 1
-        }
-     };
-    
-     const popupTemplate = {
-        title: "{Name}",
-        content: "{Description}"
-     }
-     const attributes = {
-        Name: "Graphic",
-        Description: "I am a polygon"
-     }
-    
-     const polygonGraphic = new Graphic({
-        geometry: polygon,
-        symbol: simpleFillSymbol,
-    
-        attributes: attributes,
-        popupTemplate: popupTemplate
-    
-     });
-     graphicsLayer.add(polygonGraphic);
-    
-     });
+   // Create a new MapView that will automatically display the map created above
+   // at the coordinates provided. Coordinates were included with the map URL.
+   const view = new MapView({
+       container: "viewDiv",
+       map: map,
+       extent: {
+           xmin: -13638301.6348,
+           ymin: 4538270.3041,
+           xmax: -13620715.1545,
+           ymax: 4552841.6267,
+           spatialReference: 102100
+       }
+   });
+
+   // This is a the template layer that will be used to display the schools and their info.
+   const schoolsTemplate = {
+       title: "{Campus_Name}",
+       content: "<b>Campus Name:</b>{Campus_Name} <br/> <b>Grade Range:</b> {Grade_range} <br/> <b>Category:</b> {Category} <br/> <b>Police Districts:</b> {Police_Districts}</b>",
+   };
+
+   // This is the layer that will be used to display the schools.
+   // Notice the outfields that are used in the template.
+   const sanFranciscoSchools = new FeatureLayer({
+       url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/San_Francisco_Schools/FeatureServer/0",
+       outFields: ["Campus_Name", "Grade_range", "Category", "Police_Districts"],
+       popupTemplate: schoolsTemplate
+   });
+
+   // Add the layer to the map.
+   map.add(sanFranciscoSchools, 0);
+
+   // This is the template layer that will be used to display the SF Neighborhoods and their info.
+   const popupTemplate = {
+       title: "Neighborhood",
+       content: "<b>Neighborhood:</b> {nbrhood}<br><b>Total Population:</b> {TOTPOP_CY}",
+   };
+
+   // This is the layer that will be used to display the SF Neighborhoods.
+   // Notice the outfields that are used in the template.
+   const sanFranciscoNeighborhoods = new FeatureLayer({
+       url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/SF_Neighborhoods_with_population/FeatureServer/0",
+       outFields: ["nbrhood", "TOTPOP_CY"],
+       popupTemplate: popupTemplate,
+   });
+
+   // Add the layer to the map.
+   map.add(sanFranciscoNeighborhoods, 0);
+
+});
